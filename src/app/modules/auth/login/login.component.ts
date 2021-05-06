eedfb97322686f66282f11fe95aba970c428d038
Eformeh-Bello Wisdom <oyievesho50@gmail.com>
2021-05-06 10:15:01 +0100

    login component update

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import from 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css',]
 })
 export class LoginComponent implements OnInit {
 
