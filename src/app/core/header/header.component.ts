325f11a82a58b95ef6226e86a87dc6e7d19608cc
Eformeh-Bello Wisdom <oyievesho50@gmail.com>
2021-05-20 13:51:03 +0100

    icon and image complete

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css', './unsemantic-grid-responsive-tablet.css']
 })
 export class HeaderComponent implements OnInit {
+
+ faSearch = faSearch;
 
   constructor() { }
 
