18ccf9b5b295b2b26ffe431ca49177eb09c8a022
Eformeh-Bello Wisdom <oyievesho50@gmail.com>
2021-05-26 13:51:39 +0100

    bnb

--- src/app/home/landing/landing.component.ts
+++ src/app/home/landing/landing.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['css/reset.css','css/unsemantic-grid-responsive-tablet.css','css/stylesheet.css']
+  styleUrls: ['css/reset.css','css/unsemantic-grid-responsive-tablet.css','css/stylesheet.css','css/animate.css','landing.component.css']
 })
 export class LandingComponent implements OnInit {
 
