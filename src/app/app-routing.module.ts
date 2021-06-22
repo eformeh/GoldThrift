3d89e298191726746841cccad19edad29b68c5a8
ChuksDsuaVe <chukwurahpromise94@gmail.com>
2021-06-22 14:32:29 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LandingComponent } from './home/landing/landing.component';
-import { ProductComponent } from './products/product.component';
+// import { ProductComponent } from './products/product.component';
 
 const routesApp: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full', component:LandingComponent },
